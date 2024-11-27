257523eff5ada7d1bbbbe12ab9fe52afa2d2f970
sonar <sonaraklpesh1998@gmail.com>
2024-11-27 16:41:04 +0530

    added asynchronous api app with csv files uploading

---
+++ multithreading-application/src/main/java/com/multithread/async/MultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.multithread.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingApplication.class, args);
+	}
+
+}
